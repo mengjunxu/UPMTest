5f9f69c329722faf81c99813b5a4812148634a02
mengjunxu <mengjun.xu@unity3d.com>
2021-03-18 13:56:29 +0800

    udpate

---
+++ Runtime/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CSharpTest;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text text;
+    void Start()
+    {
+        Class1 test = new Class1();
+        test.showText(text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
